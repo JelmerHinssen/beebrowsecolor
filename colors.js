c6e685f86cd9733bf3028d855e53114074cc43ef
Jelmer Hinssen <j.hinssen@student.ru.nl>
2022-10-29 15:07:06 +0200

    Refactor colors.js

--- colors.js
+++ colors.js
@@ -1,57 +1,57 @@
 import LocalStorage from './localStorage'
 import {
-  createElement,
-  createGoalLabel,
-  getGoalElements,
-  getGoalParentElement,
-  goalCmp,
-  isGoalCollapsed,
-  isGoalRed
+    getGoalElements,
+    getGoalParentElement,
 } from './util'
 
-const ColorNames = ["red", "orange", "blue", "green"];
+const COLOR_NAMES = ["red", "orange", "blue", "green"];
+const DEFAULT_COLORS = [1, 2, 3, 14];
+
 
 export default {
-  init () {
-    if (!getGoalParentElement()) return
-
-    const goalElements = getGoalElements()
-
-    goalElements.forEach(elem => {
-        let goalname = elem.getAttribute("data-slug");
-
-        let deadline = new Date(elem.getAttribute("data-losedate") * 1000);
-        let now = new Date();
-        let daysUntillDeadline = (deadline - now) / (24 * 60 * 60 * 1000);
-        console.log(daysUntillDeadline);
-        for (let x of ColorNames) {
-            elem.classList.remove(x);
-        }
-        let thumbnail = elem.querySelector("thumbnail img");
-        if (thumbnail) {
-            let title = thumbnail.getAttribute("title");
-            title = title.split(": ")[1];
-            thumbnail.setAttribute("title", title);
-        }
-        let colors = LocalStorage.loadColors({goalname});
-        if (!colors || colors.length === 0) {
-            colors = [1, 2, 3, 14];
-        }
-        let hasColor = false;
-        for (let i = 0; i < Math.min(ColorNames.length, colors.length); i++) {
-            if (daysUntillDeadline < colors[i]) {
-                elem.classList.add(ColorNames[i]);
-                hasColor = true;
-                break;
-            }
-        }
-        if (!hasColor) {
-            elem.classList.add("nocolor");
-        }
-
-    //   new Date()
-    //   console.log(new Date())
-    })
-  }
+    init() {
+        if (!getGoalParentElement()) return
+        const goalElements = getGoalElements()
+        goalElements.forEach(goal => {
+            removeColors(goal);
+            addColors(goal);
+        })
+    }
 }
 
+function getDaysUntilDeadline(goal) {
+    let { losedate } = goal.dataset;
+    let deadline = new Date(losedate * 1000);
+    let now = new Date();
+    return (deadline - now) / (24 * 60 * 60 * 1000);
+}
+
+function addColors(goal) {
+    let daysUntilDeadline = getDaysUntilDeadline(goal);
+    let colors = getGoalColors(goal);
+    let hasColor = false;
+    for (let i = 0; i < Math.min(COLOR_NAMES.length, colors.length); i++) {
+        if (daysUntilDeadline < colors[i]) {
+            goal.classList.add(COLOR_NAMES[i]);
+            hasColor = true;
+            break;
+        }
+    }
+    if (!hasColor) {
+        goal.classList.add("nocolor");
+    }
+}
+
+function removeColors(goal) {
+    for (let x of COLOR_NAMES) {
+        goal.classList.remove(x);
+    }
+}
+
+function getGoalColors(goal) {
+    let colors = LocalStorage.loadColors(goal);
+    if (!colors || colors.length === 0) {
+        colors = DEFAULT_COLORS;
+    }
+    return colors;
+}
